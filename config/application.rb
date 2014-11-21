acfd42a07d3129d54f5ce56d7d9ae24fc901a947
Yuri S <fudoshiki.ari@gmail.com>
2014-11-21 16:49:00 +0500

    fix i18n locales changer

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module KemerovoInvest
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ru
-    config.i18n.available_locales = [:ru, :en, :de, :zh]
+    config.i18n.available_locales = [:ru, :en]
     config.i18n.fallbacks = [:en]
 
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '**').to_s]
