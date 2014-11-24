faa67c198ec0e80b1a35b0f9d09041e32dbcf100
Yuri S <fudoshiki.ari@gmail.com>
2014-11-24 14:07:32 +0500

    template fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'new.mystand.ru', user: 'kemerovo-invest', roles: %w(web app db)
+server 'new.mystand.ru', user: 'admin-with-mongo', roles: %w(web app db)
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
